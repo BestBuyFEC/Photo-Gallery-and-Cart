81730789fbbdeb1e3d908b107c105c7b0af5ed1b
greyes92 <gibranreyes92@gmail.com>
2022-07-05 10:36:41 -0500

    reyes updates

--- bestbuy/migration.sql
+++ bestbuy/migration.sql
@@ -1,17 +1,26 @@
--- CREATE TABLE product(
---     id SERIAL PRIMARY KEY,
---     product_name VARCHAR,
---     sku VARCHAR,
---     release_date VARCHAR,
---     model VARCHAR,
---     price NUMERIC
--- );
+DROP DATABASE galleryandcart_db;
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS image;
+DROP TABLE IF EXISTS cart;
 
--- CREATE TABLE image(
---     id SERIAL PRIMARY KEY,
---     image_url VARCHAR,
---     product_id INT REFERENCES product(id)
--- );
+CREATE DATABASE galleryandcart_db;
+\c galleryandcart_db
+
+
+CREATE TABLE product(
+    id SERIAL PRIMARY KEY,
+    product_name VARCHAR,
+    sku VARCHAR,
+    release_date VARCHAR,
+    model VARCHAR,
+    price NUMERIC
+);
+
+CREATE TABLE image(
+    id SERIAL PRIMARY KEY,
+    image_url VARCHAR,
+    product_id INT REFERENCES product(id)
+);
 
 CREATE TABLE cart(
     id serial PRIMARY KEY,
--- bestbuy/src/components/Carousel.jsx
+++ bestbuy/src/components/Carousel.jsx
@@ -4,15 +4,15 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function PicCarousel({ images }) {
   return (
-    <>
-      <Carousel className="pic">
+    <div className="pricingCarouselContainer">
+      <Carousel className="pic" showIndicators={false} showStatus={false}>
         {images.map((images) => (
           <div>
             <img key={images.id} alt="" src={images.image_url}></img>
           </div>
         ))}
       </Carousel>
-    </>
+    </div>
   );
 }
 
